2b5cdb862ad89a713f0e177c9cc04a81435e766b
Shai Mann-Robison <mann.robison@gmail.com>
2023-06-14 12:43:27 -0400

    init

---
+++ src/main/java/com/bdsumhac/pwbd/PwbdApplication.java
@@ -0,0 +1,13 @@
+package com.bdsumhac.pwbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdsumhac/pwbd/PwbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdsumhac.pwbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
