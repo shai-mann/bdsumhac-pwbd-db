4aadb285aac4de1efeded3444ca403339a8d22a3
Shai Mann-Robison <mann.robison@gmail.com>
2023-08-15 13:27:47 -0400

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,24 +1,6 @@
-drop table facility_flags;
-
 drop table facility_edits;
 
 drop table facilities;
-
-drop table flags;
-
-drop table service_codes;
-
-create table service_codes (
-    id identity primary key,
-    code varchar(4) not null,
-    name varchar(128) not null
-);
-
-create table flags (
-    id identity primary key,
-    code varchar(4) not null,
-    description varchar(256) not null
-);
 
 create table facilities (
     id identity primary key,
@@ -32,20 +14,10 @@ create table facilities (
     zip4 int,
     county varchar(128) not null,
     phone varchar(128) not null,
-    intake_prompt varchar(128),
-    intake1 varchar(128),
-    intake2 varchar(128),
     website varchar(128) not null,
     latitude float not null,
     longitude float not null,
-    type_facility int references service_codes (id),
     pwbd boolean
-);
-
-create table facility_flags (
-    id identity primary key,
-    facility int references facilities (id),
-    flag int references flags (id)
 );
 
 create table facility_edits (
