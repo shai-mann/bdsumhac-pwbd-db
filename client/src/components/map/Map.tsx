bdc4e2e5de11d9483ee5df4b8e8dad451917832d
Shai Mann-Robison <mann.robison@gmail.com>
2024-01-08 16:16:05 -0500

    automatic re-center on query

--- client/src/components/map/Map.tsx
+++ client/src/components/map/Map.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import "./map.css";
 import { ColorRing } from "react-loader-spinner";
 import {
@@ -27,6 +27,7 @@ const FacilityMap: FC<FacilityMapProps> = ({
   facilities,
   clickCallback,
 }) => {
+  const [scaleFactor, setScaleFactor] = useState(1);
   const [zoom, setZoom] = useState(1);
   const [center, setCenter] = useState<[number, number]>([-96, 38.5]);
   const [hoveredMarker, setHoveredMarker] = useState<Facility>();
@@ -41,11 +42,39 @@ const FacilityMap: FC<FacilityMapProps> = ({
     setZoom(zoom);
   };
 
+  const updateZoom = (newZoom: number) => {
+    setZoom(newZoom);
+    setScaleFactor(newZoom);
+  };
+
+  useEffect(() => {
+    if (querying) {
+      return;
+    }
+
+    if (facilities.length === 0) {
+      setCenter([-96, 38.5]);
+      updateZoom(1);
+      return;
+    }
+
+    const latLongs = facilities.map((f) => [f.latitude, f.longitude]);
+
+    const [sumLat, sumLon] = latLongs.reduce(([lat, lon], [curLat, curLon]) => [
+      lat + curLat,
+      lon + curLon,
+    ]);
+    const newCenter = [sumLon / facilities.length, sumLat / facilities.length];
+
+    setCenter(newCenter as [number, number]);
+  }, [facilities, querying]);
+
   return (
     <div style={{ position: "relative" }}>
       <div style={{ position: "revert" }}>
         <ComposableMap projection="geoAlbers" style={{ height: 400 }}>
           <ZoomableGroup
+            onMove={({ zoom }) => setScaleFactor(zoom)}
             center={center}
             zoom={zoom}
             maxZoom={500}
@@ -70,7 +99,7 @@ const FacilityMap: FC<FacilityMapProps> = ({
                   <FacilityMarker
                     facility={f}
                     key={i}
-                    scaleFactor={zoom}
+                    scaleFactor={scaleFactor}
                     hoveredCallback={hoveredCallback}
                     clickCallback={clickCallback}
                   />
@@ -79,7 +108,7 @@ const FacilityMap: FC<FacilityMapProps> = ({
                   <FacilityMarker
                     facility={hoveredMarker}
                     key={"hovered"}
-                    scaleFactor={zoom}
+                    scaleFactor={scaleFactor}
                     hoveredCallback={hoveredCallback}
                     clickCallback={clickCallback}
                   />
@@ -103,14 +132,10 @@ const FacilityMap: FC<FacilityMapProps> = ({
         )}
       </div>
       <div style={{ position: "absolute", bottom: 0, right: 0 }}>
-        <IconButton
-          onClick={() => setZoom(zoom * 1.1)}
-        >
+        <IconButton onClick={() => updateZoom(zoom * 1.1)}>
           <PlusIcon sx={{ color: pink[400] }} fontSize="large" />
         </IconButton>
-        <IconButton
-          onClick={() => setZoom(zoom / 1.1)}
-        >
+        <IconButton onClick={() => updateZoom(zoom / 1.1)}>
           <MinusIcon sx={{ color: pink[400] }} fontSize="large" />
         </IconButton>
       </div>
